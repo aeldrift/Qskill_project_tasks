726509019048040a1892ace0e3b3ce42d5d4e8b9
Teesha <jindalteesha22@gmail.com>
2026-02-03 09:27:19 +0530

    added

--- Task2_Food_Delivery_Delay_Analysis/task2_food_delivery_analysis.py
+++ Task2_Food_Delivery_Delay_Analysis/task2_food_delivery_analysis.py
@@ -31,3 +31,25 @@ print(data.head())
 data = data.dropna()
 
 print("\nAfter Removing Missing Values:")
+print(data.info())
+
+
+
+# BASIC DATA ANALYSIS
+
+average_delivery_time = data["Delivery_time(min)"].mean()
+print("\nAverage Delivery Time (minutes):", round(average_delivery_time, 2))
+
+
+# BAR CHART
+
+traffic_delay = data.groupby(
+    "Road_traffic_density"
+)["Delivery_time(min)"].mean()
+
+plt.figure()    # Average Delivery Time by Traffic Density
+traffic_delay.plot(kind="bar")
+plt.title("Average Delivery Time by Traffic Density")
+plt.xlabel("Traffic Density")
+plt.ylabel("Average Delivery Time (minutes)")
+plt.show()
